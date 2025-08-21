cb095efc499db83f7d4004559b27511819459236
Myokant Kyaw <myokhantkyaw.developer@gmail.com>
2025-08-21 14:04:30 +0630

    Auth Change

--- backend/Model/Auth/auth.js
+++ backend/Model/Auth/auth.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  studentid: { type: String },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 }, { timestamps: true });
