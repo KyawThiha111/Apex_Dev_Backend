81e1ef114e8734da8ae77bd600d8da9c592a07c7
KyawThiha111 <143880386+KyawThiha111@users.noreply.github.com>
2025-10-14 20:06:10 -0700

    Changes

--- backend/Controller/QuizGenerator/QuizGenerator.js
+++ backend/Controller/QuizGenerator/QuizGenerator.js
@@ -7,7 +7,7 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY)
    const {subject,chapter,topic,option} = req.query;
    
   try {
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
     const prompt = `
       You are a question generator for ${subject} practice.
       The user is at chapter ${chapter} where they learn about ${topic}.
